d3f58586c9eab407e344a0e81e58681aafc174ab
Mercy Oyelude <mercyoyelude0@gmail.com>
2025-10-09 11:44:58 +0100

    feat: integrate Toaster component for enhanced notification management

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { AuthProvider } from "@/context/AuthContext";
+import { Toast, Toaster } from "react-hot-toast";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -29,6 +30,11 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <AuthProvider>{children}</AuthProvider>
+        <Toaster
+          position="top-right"
+          toastOptions={{ duration: 3000 }}
+          reverseOrder={false}
+        />
       </body>
     </html>
   );
